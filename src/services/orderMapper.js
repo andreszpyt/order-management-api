483e10096f4e72bed6c5cf2ed62f3e2473643640
andreszpyt <andre.szpyt@gmail.com>
2026-03-06 13:47:45 -0300

    feat: create order schema and data mapper service

---
+++ src/services/orderMapper.js
@@ -0,0 +1,17 @@
+function mapOrderToDatabaseFormat(requestBody) {
+    return {
+        orderId: requestBody.numeroPedido,
+        value: requestBody["valor Total"],
+        creationDate: requestBody.dataCriacao,
+
+        items: requestBody.items.map(item => {
+            return {
+                productId: parseInt(item.idItem),
+                quantity: item.quantidadeltem,
+                price: item.valorltem
+            };
+        })
+    };
+}
+
+module.exports = { mapOrderToDatabaseFormat };
