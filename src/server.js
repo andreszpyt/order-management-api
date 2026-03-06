96423caca86982d214a9ab430e2bd8c353073a6c
andreszpyt <andre.szpyt@gmail.com>
2026-03-06 14:30:38 -0300

    feat: implement order controller and setup API routes

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const orderRoutes = require('./src/routes/orderRoutes');
+const uri = process.env.MONGO_URI;
+const app = express();
+
+app.use(express.json());
+
+app.use('/', orderRoutes);
+
+mongoose.connect(uri)
+    .then(() => {
+        console.log('Conectado ao banco de dados com sucesso!');
+
+        app.listen(3000, () => {
+            console.log('Servidor rodando na porta 3000: http://localhost:3000');
+        });
+    })
+    .catch((error) => {
+        console.error('Erro ao conectar no banco de dados:', error);
+    });
